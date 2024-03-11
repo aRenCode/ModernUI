7a092bac6d3ca423d65f2c573587bdbb82d17403
aRenCode <pryvya@gmail.com>
2024-03-11 02:56:42 -0400

    A few changes before adding new things

---
+++ ModernUI/Classes/ChatMessage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernUI.Classes
+{
+    class ChatMessage
+    {
+		private string username;
+
+		public string Username
+		{
+			get { return this.username; }
+			set { username = value; }
+		}
+
+        private string message;
+
+        public string Message
+        {
+            get { return this.message; }
+            set { message = value; }
+        }
+
+
+        private string colorValue;
+
+        public string ColorValue
+        {
+            get { return this.colorValue; }
+            set { colorValue = value; }
+        }
+
+    }
+}
